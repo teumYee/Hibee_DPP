e22261401c37f027cdabac1d7fb7237900ea000a
이승현 <teumteumee@gmail.com>
2026-02-06 22:36:50 +0900

    fix: 대시보드 api 로직 개선

--- DPP/DPP_BE/app/api/v1/endpoints/dashboard.py
+++ DPP/DPP_BE/app/api/v1/endpoints/dashboard.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import func,desc,text
 from sqlalchemy.orm import Session
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from typing import List, Optional
 
 from app import schemas
@@ -17,25 +17,30 @@ router = APIRouter()
 def get_dashboard_summary(user_id: int, db: Session = Depends(get_db)):
     # today = datetime.now().date() 
     # test_date = date(2026, 1, 31) 
-    test_date = datetime.now().date()
     # logs = db.query(UsageLog).filter(func.date(UsageLog.date) == test_date).all()
+    today_start = datetime.combine(date.today(), datetime.min.time())
+    next_day_start = today_start + timedelta(days=1)
     
     # 1. 상단 카드 지표 (총 시간, 총 언락)
     stats = db.query(
-        func.sum(UsageLog.usage_duration).label("total_time"),
+        func.max(UsageLog.usage_duration).label("total_time"),
         func.max(UsageLog.unlock_count).label("total_unlocks")
     ).filter(
         UsageLog.user_id == user_id, 
-        func.date(UsageLog.date)==test_date).first()
+        UsageLog.date >= today_start,
+        UsageLog.date < next_day_start
+    ).first()
 
     # 2. 가장 자주 들른 곳 & 상위 3개 앱
     #  app_launch_count 필드를 합산하여 순위를 매김.
     app_ranks = db.query(
         UsageLog.package_name,
         UsageLog.app_name, # 앱 이름도 같이 가져오기
-        func.sum(UsageLog.app_launch_count).label("launch_count"),
-        func.sum(UsageLog.usage_duration).label("total_duration")
-    ).filter(UsageLog.user_id == user_id, func.date(UsageLog.date)== test_date)\
+        func.max(UsageLog.app_launch_count).label("launch_count"),
+        func.max(UsageLog.usage_duration).label("total_duration")
+    ).filter(UsageLog.user_id == user_id, 
+        UsageLog.date >= today_start,
+        UsageLog.date < next_day_start)\
      .group_by(UsageLog.package_name, UsageLog.app_name)\
      .order_by(
      desc(text("launch_count")),    
@@ -44,8 +49,8 @@ def get_dashboard_summary(user_id: int, db: Session = Depends(get_db)):
 
     # 3. 가장 긴 연속 사용 시간
     max_session = db.query(func.max(UsageLog.max_continuous_duration))\
-        .filter(UsageLog.user_id == user_id, func.date(UsageLog.date)==test_date).scalar() or 0
-    
+        .filter(UsageLog.user_id == user_id, 
+                UsageLog.date >= today_start, UsageLog.date < next_day_start).scalar() or 0
     # 총 사용 시간, 언락
     total_time_hour=(stats.total_time /3600) if stats and stats.total_time else 0
     total_unlocks = stats.total_unlocks if stats and stats.total_unlocks else 0
@@ -58,11 +63,14 @@ def get_dashboard_summary(user_id: int, db: Session = Depends(get_db)):
         },
         "top_visited": {
             "main": {
-                "name": app_ranks[0].app_name if app_ranks else "데이터 없음",
-                "count": int(app_ranks[0].launch_count) if app_ranks else 0
+                "name": app_ranks[0].app_name if app_ranks and app_ranks[0].app_name != "Unknown" else (app_ranks[0].package_name if app_ranks else "데이터 없음"),
+                "count": int(app_ranks[0].launch_count) if app_ranks else 0     
             },
             "others": [
-                {"name": row.app_name, "count": int(row.launch_count)} for row in app_ranks[1:4]
+                {
+                    "name": row.app_name if row.app_name != "Unknown" else row.package_name, 
+                    "count": int(row.launch_count)
+                } for row in app_ranks[1:4]
             ]
         }
     }
